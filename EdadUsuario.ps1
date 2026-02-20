18a968d03753a22729193a05346bf30acc091f60
kev312 <156649847+kev312@users.noreply.github.com>
2026-02-20 15:51:05 +0100

    Add files via upload

---
+++ EdadUsuario.ps1
@@ -0,0 +1,7 @@
+﻿do {
+    $edad = Read-Host "Edad"; $origen = Read-Host "Directorio origen"; $destino = Read-Host "Directorio destino"
+    if ($edad -ge 18) {Copy-Item -Path $origen -Destination $destino -Recurse -Force; Get-ChildItem -Path $destino}
+    elseif ($edad -gt 10) {Write-Host ($origen + $destino)}
+    else {Write-Host $origen}
+    $input = Read-Host "¿Continuar? (s/n)"
+} until ($input -eq 'n')
